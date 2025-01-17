9f8a44e41d1605692f31e6e48eed49909137d95e
21084548@sigma.sbrf.ru <YGKiselev@sberbank.ru>
2025-01-17 22:46:21 +0300

    ecs wip - visitor1 for refs

--- rg_ecs/src/visitor.rs
+++ rg_ecs/src/visitor.rs
@@ -38,13 +38,61 @@ impl<T: 'static> CompRef for &mut T {
 trait Locker {
     type Ty: 'static;
     type Guard<'g>;
-    type Iter<'i>: Iterator;
+    type Item<'r>;
+    type Iter<'i>: Iterator<Item = Self::Item<'i>>;
 
     fn lock(chunk: &Chunk) -> Self::Guard<'_>;
 
     fn iter<'a>(guard: &'a mut Self::Guard<'_>) -> Self::Iter<'a>;
+
 }
 
+impl<T> Locker for &mut T
+where
+    T: 'static,
+{
+    type Ty = T;
+    type Guard<'g> = RwLockWriteGuard<'g, Box<dyn ComponentStorage>>;
+    type Item<'r> = &'r mut T;
+    type Iter<'i> = core::slice::IterMut<'i, T>;
+
+    fn lock(chunk: &Chunk) -> Self::Guard<'_> {
+        chunk
+            .get_column(ComponentId::new::<T>())
+            .unwrap()
+            .write()
+            .unwrap()
+    }
+
+    fn iter<'a>(guard: &'a mut Self::Guard<'_>) -> Self::Iter<'a> {
+        cast_mut::<T>(guard.as_mut()).iter_mut()
+    }
+}
+
+impl<T> Locker for &T
+where
+    T: 'static,
+{
+    type Ty = T;
+    type Guard<'g> = RwLockReadGuard<'g, Box<dyn ComponentStorage>>;
+    type Item<'r> = &'r T;
+    type Iter<'i> = core::slice::Iter<'i, T>;
+
+    fn lock(chunk: &Chunk) -> Self::Guard<'_> {
+        chunk
+            .get_column(ComponentId::new::<T>())
+            .unwrap()
+            .read()
+            .unwrap()
+    }
+
+    fn iter<'a>(guard: &'a mut Self::Guard<'_>) -> Self::Iter<'a> {
+        cast::<T>(guard.as_ref()).iter()
+    }
+}
+
+
+/*
 struct MutRef<T> {
     _phantom: PhantomData<T>,
 }
@@ -55,7 +103,7 @@ where
 {
     type Ty = T;
     type Guard<'g> = RwLockWriteGuard<'g, Box<dyn ComponentStorage>>;
-
+    type Item<'r> = &'r mut T;
     type Iter<'i> = core::slice::IterMut<'i, T>;
 
     fn lock(chunk: &Chunk) -> Self::Guard<'_> {
@@ -77,6 +125,39 @@ impl<T: 'static> CompRef for MutRef<T> {
     }
 }
 
+struct Ref<T> {
+    _phantom: PhantomData<T>,
+}
+
+impl<T> Locker for Ref<T>
+where
+    T: 'static,
+{
+    type Ty = T;
+    type Guard<'g> = RwLockReadGuard<'g, Box<dyn ComponentStorage>>;
+    type Item<'r> = &'r T;
+    type Iter<'i> = core::slice::Iter<'i, T>;
+
+    fn lock(chunk: &Chunk) -> Self::Guard<'_> {
+        chunk
+            .get_column(ComponentId::new::<T>())
+            .unwrap()
+            .read()
+            .unwrap()
+    }
+
+    fn iter<'a>(guard: &'a mut Self::Guard<'_>) -> Self::Iter<'a> {
+        cast::<T>(guard.as_ref()).iter()
+    }
+}
+
+impl<T: 'static> CompRef for Ref<T> {
+    fn component_id() -> ComponentId {
+        ComponentId::new::<T>()
+    }
+}
+*/
+
 ///
 /// Visitor1
 ///
@@ -88,7 +169,7 @@ struct Visitor1<A, H> {
 
 impl<A, H> Visitor1<A, H>
 where
-    H: Fn(A::Iter<'_>),
+    H: Fn(A::Item<'_>),
     A: Locker,
 {
     fn new(handler: H) -> Self {
@@ -107,37 +188,38 @@ where
     fn visit(&self, chunk: &Chunk) {
         let mut guard1 = A::lock(chunk);
         let mut it1 = A::iter(&mut guard1);
-        (self.handler)(it1);
+        while let Some(v1) = it1.next() {
+            (self.handler)(v1);
+        }
     }
 }
 
 #[cfg(test)]
 mod test {
 
-    use std::{
-        alloc::Layout,
-        any::Any,
-        marker::PhantomData,
-        ops::{Deref, DerefMut},
-        sync::{Arc, Mutex},
-    };
-
     use crate::{archetype::ArchetypeStorage, build_archetype, entity::EntityId};
 
-    use super::{CompRef, MutRef, Visitor1};
+    use super::{Visitor1};
 
     #[test]
     fn visitor() {
-        let vis = Visitor1::<MutRef<i32>, _>::new(|mut it1| {
-            while let Some(v1) = it1.next() {
-                dbg!(v1);
-            }
-        });
-
         let mut storage = ArchetypeStorage::new(build_archetype![String, f64, bool, i32], 1000);
-        for i in 0..1000 {
+        for i in 0..5 {
             storage.add(EntityId::new(i));
         }
+
+        let vis = Visitor1::<&mut i32, _>::new(|v1| {
+            dbg!(v1);
+        });
+
+        for chunk in storage.iter() {
+            vis.visit(chunk);
+        }
+
+        let vis = Visitor1::<&f64, _>::new(|v1| {
+            dbg!(v1);
+        });
+
         for chunk in storage.iter() {
             vis.visit(chunk);
         }
