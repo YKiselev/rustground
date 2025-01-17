0b9c3905bb783f82c5ad8483f99712ce5eb4e079
21084548@sigma.sbrf.ru <YGKiselev@sberbank.ru>
2025-01-17 23:15:00 +0300

    ecs visitor2

--- rg_ecs/src/visitor.rs
+++ rg_ecs/src/visitor.rs
@@ -44,7 +44,6 @@ trait Locker {
     fn lock(chunk: &Chunk) -> Self::Guard<'_>;
 
     fn iter<'a>(guard: &'a mut Self::Guard<'_>) -> Self::Iter<'a>;
-
 }
 
 impl<T> Locker for &mut T
@@ -90,7 +89,6 @@ where
         cast::<T>(guard.as_ref()).iter()
     }
 }
-
 
 /*
 struct MutRef<T> {
@@ -158,6 +156,10 @@ impl<T: 'static> CompRef for Ref<T> {
 }
 */
 
+fn comp_id<L: Locker>() -> ComponentId {
+    ComponentId::new::<L::Ty>()
+}
+
 ///
 /// Visitor1
 ///
@@ -174,7 +176,7 @@ where
 {
     fn new(handler: H) -> Self {
         Visitor1 {
-            component: ComponentId::new::<A::Ty>(),
+            component: comp_id::<A>(),
             handler,
             _phantom: PhantomData::default(),
         }
@@ -194,15 +196,53 @@ where
     }
 }
 
+///
+/// Visitor2
+///
+struct Visitor2<A, B, H> {
+    components: Vec<ComponentId>,
+    handler: H,
+    _phantom: PhantomData<(A, B)>,
+}
+
+impl<A, B, H> Visitor2<A, B, H>
+where
+    H: Fn(A::Item<'_>, B::Item<'_>),
+    A: Locker,
+    B: Locker,
+{
+    fn new(handler: H) -> Self {
+        Visitor2 {
+            components: vec![comp_id::<A>(), comp_id::<B>()],
+            handler,
+            _phantom: PhantomData::default(),
+        }
+    }
+
+    fn accept(&self, columns: &HashSet<ComponentId>) -> bool {
+        self.components.iter().all(|c| columns.contains(c))
+    }
+
+    fn visit(&self, chunk: &Chunk) {
+        let mut guard1 = A::lock(chunk);
+        let mut guard2 = B::lock(chunk);
+        let mut it1 = A::iter(&mut guard1);
+        let mut it2 = B::iter(&mut guard2);
+        while let (Some(v1), Some(v2)) = (it1.next(), it2.next()) {
+            (self.handler)(v1, v2);
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
 
     use crate::{archetype::ArchetypeStorage, build_archetype, entity::EntityId};
 
-    use super::{Visitor1};
+    use super::{Visitor1, Visitor2};
 
     #[test]
-    fn visitor() {
+    fn visitor1() {
         let mut storage = ArchetypeStorage::new(build_archetype![String, f64, bool, i32], 1000);
         for i in 0..5 {
             storage.add(EntityId::new(i));
@@ -218,6 +258,22 @@ mod test {
 
         let vis = Visitor1::<&f64, _>::new(|v1| {
             dbg!(v1);
+        });
+
+        for chunk in storage.iter() {
+            vis.visit(chunk);
+        }
+    }
+
+    #[test]
+    fn visitor2() {
+        let mut storage = ArchetypeStorage::new(build_archetype![String, f64, bool, i32], 1000);
+        for i in 0..5 {
+            storage.add(EntityId::new(i));
+        }
+
+        let vis = Visitor2::<&mut i32, &f64, _>::new(|v1, v2| {
+            dbg!(v1, v2);
         });
 
         for chunk in storage.iter() {
